818e6dd4ebe30d617770d8c780dd7c2794792969
rlagkdud <ha0936861@gmail.com>
2019-12-11 19:43:56 +0900

    lab 10

--- lab10/book_search.js
+++ lab10/book_search.js
@@ -1,9 +1,23 @@
 window.onload = function() {
     $("b_xml").onclick=function(){
-    	    //construct a Prototype Ajax.request object
+			//construct a Prototype Ajax.request object
+			new Ajax.Request("books.php",{
+				method:"get",
+				parameters:{category:getCheckedRadio($$('input'))},
+				onSuccess:showBooks_XML,
+				onFailure:ajaxFailed,
+				onException:ajaxFailed
+			})
     }
     $("b_json").onclick=function(){
-    	    //construct a Prototype Ajax.request object
+			//construct a Prototype Ajax.request object
+			new Ajax.Request("books_json.php", {
+				method: "get",
+				parameters: {category: getCheckedRadio($$('input'))},
+				onSuccess: showBooks_JSON,
+				onFailure: ajaxFailed,
+				onException: ajaxFailed
+			})
     }
 };
 
@@ -17,11 +31,29 @@ function getCheckedRadio(radio_button){
 }
 
 function showBooks_XML(ajax) {
-	alert(ajax.responseText);
+	//  alert(ajax.responseText);
+	var books = ajax.responseXML.getElementsByTagName("book");
+	var ul = $("books");
+	ul.innerHTML="";
+	for(var i = 0; i<books.length; i++){
+		var title = books[i].getElementsByTagName("title")[0].firstChild.nodeValue;
+		var author = books[i].getElementsByTagName("author")[0].firstChild.nodeValue;
+		var year = books[i].getElementsByTagName("year")[0].firstChild.nodeValue;
+		var li = document.createElement("li");
+		li.innerHTML = `${title}, by${author} (${year})`;
+		ul.appendChild(li);
+		// ul.innerHTML+='<li>'+title+','+author+','+year+','+'</li>';
+
+	}
 }
 
 function showBooks_JSON(ajax) {
-	alert(ajax.responseText);
+	var data = JSON.parse(ajax.responseText);
+	var ul =  $("books")
+	ul.innerHTML = ""
+	for (var i=0; i<data.books.length; i++) {
+		ul.innerHTML += "<li>" + data.books[i].title + ", by " + data.books[i].author + " (" + data.books[i].year + ") </li>";
+	}
 }
 
 function ajaxFailed(ajax, exception) {
--- lab10/books_json.php
+++ lab10/books_json.php
@@ -1,43 +1,49 @@
 <?php
 $BOOKS_FILE = "books.txt";
-
 function filter_chars($str) {
 	return preg_replace("/[^A-Za-z0-9_]*/", "", $str);
 }
-
 if (!isset($_SERVER["REQUEST_METHOD"]) || $_SERVER["REQUEST_METHOD"] != "GET") {
 	header("HTTP/1.1 400 Invalid Request");
 	die("ERROR 400: Invalid request - This service accepts only GET requests.");
 }
-
 $category = "";
 $delay = 0;
-
 if (isset($_REQUEST["category"])) {
 	$category = filter_chars($_REQUEST["category"]);
 }
 if (isset($_REQUEST["delay"])) {
 	$delay = max(0, min(60, (int) filter_chars($_REQUEST["delay"])));
 }
-
 if ($delay > 0) {
 	sleep($delay);
 }
-
 if (!file_exists($BOOKS_FILE)) {
 	header("HTTP/1.1 500 Server Error");
 	die("ERROR 500: Server error - Unable to read input file: $BOOKS_FILE");
 }
-
 header("Content-type: application/json");
-
 print "{\n  \"books\": [\n";
-
 // write a code to : 
 // 1. read the "books.txt"
 // 2. search all the books that matches the given category 
 // 3. generate the result in JSON data format 
-
+$lines = file($BOOKS_FILE);
+for ($i = 0; $i < count($lines); $i++) {
+	list($title, $author, $book_category, $year, $price) = explode("|", trim($lines[$i]));
+	if ($book_category == $category) {
+		print "\t\t{\n";
+		print "\t\t\t\"title\" : \"$title\",\n";
+		print "\t\t\t\"author\" : \"$author\",\n";
+		print "\t\t\t\"category\" : \"$category\",\n";
+		print "\t\t\t\"year\" : $year,\n";
+		print "\t\t\t\"price\" : $price\n";
+		if ($i == count($lines) -1) {
+			print "\t\t}\n";
+		} else {
+			print "\t\t},\n";
+		}
+	}
+}
 print "  ]\n}\n";
-
 ?>
